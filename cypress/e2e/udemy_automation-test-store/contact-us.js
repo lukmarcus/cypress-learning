993a9d54143abb6cc181eba7dbff1b5f8af54297
Marek Szumny <lukmarcus@gmail.com>
2023-03-12 21:37:37 +0100

    TestStore better selectors and end url check

--- cypress/e2e/udemy_automation-test-store/contact-us.js
+++ cypress/e2e/udemy_automation-test-store/contact-us.js
@@ -1,14 +1,18 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
+/// <reference types="cypress-xpath" />
 
 describe("Test Contact Us for via Automation Test Store", () => {
     it("Should be able to submit a successful submission via contact us form and go back to front page", () => {
             cy.visit('https://automationteststore.com/')
-            cy.get('a[href="https://automationteststore.com/index.php?rt=content/contact"]').click()
-            cy.get('#ContactUsFrm_first_name').type('Jan')
-            cy.get('#ContactUsFrm_email').type('jan@kowalski.pl')
-            cy.get('#ContactUsFrm_enquiry').type('Enquiry from Jan Kowalski')
-            cy.get('.col-md-6 > .btn').click()
-            cy.contains('*', "Your enquiry has been successfully sent to the store owner!")
-            cy.get('#maincontainer [href="https://automationteststore.com/"]').click()
+            //cy.xpath('//a[contains(@href,"/contact")]').click()
+            cy.get('a[href$="contact"]').click()
+            cy.get('#ContactUsFrm_first_name').type("Jan")
+            cy.get('#ContactUsFrm_email').type("jan@kowalski.pl")
+            cy.get('#ContactUsFrm_enquiry').type("Enquiry from Jan Kowalski")
+            //cy.xpath('//button[@title="Submit"]').click()
+            cy.get('button[title="Submit"]').click()
+            cy.contains('#maincontainer', "Your enquiry has been successfully sent to the store owner!")
+            cy.get('a[title="Continue"]').click()
+            cy.url().should('eq', 'https://automationteststore.com/')
     })
 })
