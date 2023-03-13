cbcc01c5d1153ab9918966194539ea8ed45e3477
Marek Szumny <lukmarcus@gmail.com>
2023-03-13 13:55:41 +0100

    contains assertion

--- cypress/e2e/udemy_webdriver-uni/contact-us.js
+++ cypress/e2e/udemy_webdriver-uni/contact-us.js
@@ -6,7 +6,7 @@ describe("Test Contact Us for via WebdriverUni", () => {
             //cy.get('#contact-us').click({force: true})
     })
 
-    it.only("Should be able to submit a successful submission via contact us form", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
             cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
             cy.get('input[name="first_name"]').should('have.attr', 'placeholder', 'First Name')
             cy.get('input[name="first_name"]').type('Jan')
@@ -21,13 +21,14 @@ describe("Test Contact Us for via WebdriverUni", () => {
             cy.get('h1').should('have.text', 'Thank You for your Message!')
     })
 
-    it("Should not be able to submit a successful submission via contact us form without first name", () => {
+    it("Should not be able to submit a successful submission via contact us form without any information", () => {
             cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
             //cy.get('input[name="first_name"]').type('Jan')
-            cy.get('input[name="last_name"]').type('Kowalski')
-            cy.get('input[name="email"]').type('jan@kowalski.pl')
-            cy.get('textarea[name="message"]').type('Comments from Jan Kowalski')
+            //cy.get('input[name="last_name"]').type('Kowalski')
+            //cy.get('input[name="email"]').type('jan@kowalski.pl')
+            //cy.get('textarea[name="message"]').type('Comments from Jan Kowalski')
             cy.get('input[type="submit"]').click()
-            cy.contains("body", "Error: all fields are required")       
+            cy.get("body").contains("Error: all fields are required")
+            cy.get("body").contains("Error: Invalid email address")
     })
 })
