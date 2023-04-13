99746dbe2ec806df140757983c7646d25b82723c
Marek Szumny <lukmarcus@gmail.com>
2023-04-13 15:22:10 +0200

    Real World Testing with Cypress update

--- cypress/e2e/cypressCourseApp/home.cy.ts
+++ cypress/e2e/cypressCourseApp/home.cy.ts
@@ -3,28 +3,28 @@ describe('Home page', () => {
     cy.visit('http://localhost:3000')
   })
 
-context('Hero section', () => {
-  it('has the h1 with the correct text', () => {
-    cy.getByData('hero-heading')
-      .should('exist')
-      .contains('Testing Next.js Applications with Cypress')
-  })
+  context('Hero section', () => {
+    it('has the h1 with the correct text', () => {
+      cy.getByData('hero-heading')
+        .should('exist')
+        .contains('Testing Next.js Applications with Cypress')
+    })
 
-  it('has the correct features', () => {
-    cy.get('dt')
-      .eq(0)
-      .contains('4 Courses')
-    cy.get('dt')
-      .eq(1)
-      .contains('25+ Lessons')
-    cy.get('dt')
-      .eq(2)
-      .contains('Free and Open Source')
+    it('has the correct features', () => {
+      cy.get('dt')
+        .eq(0)
+        .contains('4 Courses')
+      cy.get('dt')
+        .eq(1)
+        .contains('25+ Lessons')
+      cy.get('dt')
+        .eq(2)
+        .contains('Free and Open Source')
     })
   })
 
-context('Courses section', () => {
-  it('Course: Testing Your First Next.js Application', () => {
+  context('Courses section', () => {
+    it('Course: Testing Your First Next.js Application', () => {
       cy.getByData('course-0')
         .find('a')
         .eq(3)
@@ -33,7 +33,7 @@ context('Courses section', () => {
         .should('eq', '/testing-your-first-application')
     })
 
-  it('Course: Testing Foundations', () => {
+    it('Course: Testing Foundations', () => {
       cy.getByData('course-1')
         .find('a')
         .eq(3)
@@ -42,7 +42,7 @@ context('Courses section', () => {
         .should('eq', '/testing-foundations')
     })
 
-  it('Course: Cypress Fundamentals', () => {
+    it('Course: Cypress Fundamentals', () => {
       cy.getByData('course-2')
         .find('a')
         .eq(3)
@@ -51,5 +51,4 @@ context('Courses section', () => {
         .should('eq', '/cypress-fundamentals')
     })
   })
-
 })
